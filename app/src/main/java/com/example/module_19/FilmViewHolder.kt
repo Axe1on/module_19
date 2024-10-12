43f844a7995c9b9994c094fb03025edabe1a4d66
Edgar <Meccu9i@gmail.com>
2024-10-12 19:31:13 +0300

    Добавил CustomView

--- app/src/main/java/com/example/module_19/FilmViewHolder.kt
+++ app/src/main/java/com/example/module_19/FilmViewHolder.kt
@@ -5,6 +5,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.module_19.customview.RatingDonutView
 
 //В конструктор класс передается layout, который мы создали(film_item.xml)
 class FilmViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -12,6 +13,8 @@ class FilmViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val title = itemView.findViewById<TextView>(R.id.title)
     private val poster = itemView.findViewById<ImageView>(R.id.poster)
     private val description = itemView.findViewById<TextView>(R.id.description)
+    //Вот здесь мы находим в верстке наш прогресс бар для рейтинга
+    private val ratingDonut = itemView.findViewById<RatingDonutView>(R.id.rating_donut)
 
     //В этом методе кладем данные из film в наши view
     fun bind(film: Film) {
@@ -29,5 +32,7 @@ class FilmViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
             .into(poster)
         //Устанавливаем описание
         description.text = film.description
+        //Устанавливаем рэйтинг
+        ratingDonut.setProgress((film.rating * 10).toInt())
     }
 }
